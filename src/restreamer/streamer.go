6ecb00e4e23490cec755a5a3ba953a1f6e1c48ac
Gregor Riepl <onitake@gmail.com>
2017-04-21 08:51:24 +0200

    WIP Use channel closure for shutdown notification

--- src/restreamer/streamer.go
+++ src/restreamer/streamer.go
@@ -41,24 +41,22 @@ var (
 // distributing received packets on the input queue to the output queues.
 // It also handles and manages HTTP connections when added to an HTTP server.
 type Streamer struct {
-	// input is the input queue, accepting packets
+	// input is the input queue, accepting packets.
+	// When closed, streamer is stopped and all outgoing queues along with it.
 	input <-chan Packet
 	// lock is the outgoing connection pool lock
-	lock sync.RWMutex
+	lock sync.Mutex
 	// connections is the outgoing connection pool
 	connections map[*Connection]bool
-	// shutdown is the shutdown notification channel.
-	// Send true to close all downstream connections and stop the multiplier.
-	shutdown chan bool
-	// running is the global running state flag;
-	// setting it to false causes a shutdown on the
-	// the next queue run - but you should not do this.
-	// Better just use the shutdown channel.
-	running bool
 	// broker is a global connection broker
 	broker ConnectionBroker
-	// queueSize defines the maximum number of packets to queue per per connection
+	// queueSize defines the maximum number of packets to queue per outgoing connection
 	queueSize int
+	// running reflects the state of the stream: if true, incoming connections
+	// are accepted but it's not possible to relaunch.
+	// If false, incoming connections are blocked, and Stream() needs to be
+	// called to start streaming.
+	running bool
 	// stats is the statistics collector for this stream
 	stats Collector
 	// logger is a json logger
@@ -82,14 +80,13 @@ type ConnectionBroker interface {
 // qsize is the length of each connection's queue (in packets).
 // broker handles policy enforcement
 // stats is a statistics collector object.
-func NewStreamer(queue <-chan Packet, qsize uint, broker ConnectionBroker) (*Streamer) {
+func NewStreamer(qsize uint, broker ConnectionBroker) (*Streamer) {
 	streamer := &Streamer{
 		input: queue,
 		connections: make(map[*Connection]bool),
-		shutdown: make(chan bool),
-		running: false,
 		broker: broker,
 		queueSize: int(qsize),
+		running: false,
 		stats: &DummyCollector{},
 		logger: &DummyLogger{},
 	}
@@ -106,86 +103,80 @@ func (streamer *Streamer) SetCollector(stats Collector) {
 	streamer.stats = stats
 }
 
-// Close shuts down the streamer and all incoming connections.
-// The stream is restartable, you may call Connect() again later.
+// Stream is the main stream multiplier loop.
+// It reads data from the input queue and distributes it to the connections.
 //
-// Most effects of the shutdown are immediate, but it may take a moment until
-// all downstream connections are closed.
-// Calling Connect() right after Close() returns should be safe, however.
-func (streamer *Streamer) Close() error {
-	if (streamer.running) {
-		// prevent further connections
-		streamer.running = false
-		// signal shutdown
-		streamer.shutdown<- true
-		
-		// acquire the exclusive lock
-		streamer.lock.Lock()
-		// clean up
-		for conn, _ := range streamer.connections {
-			conn.Close()
-		}
-		// reset the connection pool
-		streamer.connections = make(map[*Connection]bool)
+// This routine will block; you should run it asynchronously like this:
+//
+// queue := make(chan Packet, inputQueueSize)
+// go func() {
+//   log.Fatal(streamer.Stream(queue))
+// }
+//
+// or simply:
+//
+// go streamer.Stream(queue)
+func (streamer *Streamer) Stream(queue <-chan Packet) error {
+	// interlock and check for availability first
+	streamer.lock.Lock()
+	if streamer.running {
 		streamer.lock.Unlock()
-		
-		return nil
+		return ErrAlreadyRunning
 	}
-	return ErrNotRunning
-}
-
-// Connect starts serving and streaming.
-func (streamer *Streamer) Connect() error {
-	if (!streamer.running) {
-		streamer.running = true
-		go streamer.stream()
-		return nil
-	}
-	return ErrAlreadyRunning
-}
-
-// stream is the internal stream multiplier loop.
-// It reads data from the input and distributes it to the connections.
-// Will be called asynchronously from Connect()
-func (streamer *Streamer) stream() {
+	streamer.running = true
+	streamer.lock.Unlock()
+	
 	log.Printf("Starting streaming")
-	// local run flag to avoid race conditions
+	
+	// loop until the input channel is closed
 	running := true
-	for running && streamer.running {
+	for running {
 		select {
-			case packet := <-streamer.input:
-				// got a packet, distribute
-				//log.Printf("Got packet (length %d):\n%s\n", len(packet), hex.Dump(packet))
-				
-				// content distribution only, lock in non-exclusive read mode
-				streamer.lock.RLock()
-				for conn, _ := range streamer.connections {
-					select {
-						case conn.Queue<- packet:
-							// packet distributed, done
-							//log.Printf("Queued packet (length %d):\n%s\n", len(packet), hex.Dump(packet))
-							
-							// report the packet
-							streamer.stats.PacketSent()
-						default:
-							// queue is full
-							//log.Print(ErrSlowRead)
-							
-							// report the drop
-							streamer.stats.PacketDropped()
+			case packet, err := <-queue:
+				if err == nil {
+					// got a packet, distribute
+					//log.Printf("Got packet (length %d):\n%s\n", len(packet), hex.Dump(packet))
+					
+					// protect access to the connection pool
+					streamer.lock.Lock()
+					for conn, _ := range streamer.connections {
+						select {
+							case conn.Queue<- packet:
+								// packet distributed, done
+								//log.Printf("Queued packet (length %d):\n%s\n", len(packet), hex.Dump(packet))
+								
+								// report the packet
+								streamer.stats.PacketSent()
+							default:
+								// queue is full
+								//log.Print(ErrSlowRead)
+								
+								// report the drop
+								streamer.stats.PacketDropped()
+						}
 					}
+					streamer.lock.Unlock()
+				} else {
+					// channel closed, exit
+					running = false
+					// and stop everything
+					streamer.lock.Lock()
+					streamer.running = false
+					streamer.lock.Unlock()
 				}
-				streamer.lock.RUnlock()
-			case <-streamer.shutdown:
-				// stop no matter what the global state is
-				running = false
 		}
 	}
-	// drain the shutdown channel
-	select {
-		case <-streamer.shutdown:
-		default:
+	
+	// wait for competitors before cleaning up the pool
+	streamer.lock.Lock()
+	// clean up
+	for conn, _ := range streamer.connections {
+		conn.Close()
 	}
+	// reset the connection pool
+	streamer.connections = make(map[*Connection]bool)
+	streamer.lock.Unlock()
+	
 	log.Printf("Ending streaming")
 }
 
