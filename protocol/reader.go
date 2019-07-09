c9d324a07d6aa8f9179564523426723b0142de5a
Gregor Riepl <onitake@gmail.com>
2019-07-09 08:21:36 +0200

    Added fixed-size packet reader

---
+++ protocol/reader.go
@@ -0,0 +1,86 @@
+/* Copyright (c) 2019 Gregor Riepl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package protocol
+
+import (
+	"io"
+	"bytes"
+)
+
+// FixedReader implements a buffered reader that always reads a fixed amount
+// of data from an underlying io.Reader.
+//
+// It is intended to produce a stream from a packet-based network socket, such
+// as net.UDPConn.
+//
+// Note that packet-based sockets normally give no guarantee about the order of
+// incoming packets, and don't account for resends of dropped ones.
+// If order is important, reordering must be implemented by other means.
+//
+// If the underlying reader implements the io.Closer interface, Close() calls
+// will be forwarded. Otherwise, Close() is a no-op.
+type FixedReader struct {
+	reader io.Reader
+	packetSize int
+	buffer *bytes.Buffer
+}
+
+// NewPacketBuffer creates a new buffered reader that pulls in data from an
+// io.Reader in chunks of psize bytes.
+func NewFixedReader(reader io.Reader, psize int) *FixedReader {
+	return &FixedReader{
+		reader: reader,
+		packetSize: psize,
+		buffer: bytes.NewBuffer(make([]byte, 0, psize)),
+	}
+}
+
+// Read reads as many bytes from the internal buffer as can fit into p.
+//
+// If the buffer has no data left, it tries to pull in a new packet from the
+// underlying reader.
+func (b *FixedReader) Read(p []byte) (n int, err error) {
+	// check if we need to read another packet
+	if b.buffer.Len() == 0 {
+		// read the next packet
+		p := make([]byte, b.packetSize)
+		var m int
+		// pass on err if the read fails
+		m, err = b.reader.Read(p)
+		// only buffer as many bytes as were received
+		b.buffer.Write(p[:m])
+	}
+	if err == nil {
+		// if the was no I/O error, pass on any buffer errors
+		n, err = b.buffer.Read(p)
+	} else {
+		// ignore buffer errors and pass on the I/O error instead
+		n, _ = b.buffer.Read(p)
+	}
+	return n, err
+}
+
+// Close closes the underlying reader.
+//
+// Subsequent Read calls will succeed as long as the internal buffer still
+// has data. If the buffer is drained, Read returns an error.
+func (b *FixedReader) Close() error {
+	if closer, ok := b.reader.(io.Closer); ok {
+		return closer.Close()
+	}
+	return nil
+}
---
+++ protocol/reader_test.go
@@ -0,0 +1,210 @@
+/* Copyright (c) 2018 Gregor Riepl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package protocol
+
+import (
+	"io"
+	"bytes"
+	"testing"
+)
+
+func TestFixedReaderNoData(t *testing.T) {
+	d := make([]byte, 0)
+	r := bytes.NewBuffer(d)
+	f := NewFixedReader(r, 10)
+	for i := 0; i < 6; i++ {
+		g := make([]byte, 2)
+		n, err := f.Read(g)
+		if n != 0 || err == nil {
+			t.Fatal("Expected 0 bytes and error")
+		}
+	}
+}
+
+func TestFixedReaderNotEnoughInput(t *testing.T) {
+	d := make([]byte, 2)
+	for i := 0; i < len(d); i++ {
+		d[i] = byte(i)
+	}
+	r := bytes.NewBuffer(d)
+	f := NewFixedReader(r, 10)
+	g := make([]byte, 2)
+	n, err := f.Read(g)
+	if n != 2 || err != nil {
+		t.Fatal("Expected 2 bytes and no error")
+	}
+	for i := 0; i < 6; i++ {
+		g := make([]byte, 2)
+		n, err := f.Read(g)
+		if n != 0 || err == nil {
+			t.Fatal("Expected 0 bytes and error")
+		}
+	}
+}
+
+func TestFixedReaderJustRight(t *testing.T) {
+	d := make([]byte, 10)
+	for i := 0; i < len(d); i++ {
+		d[i] = byte(i)
+	}
+	r := bytes.NewBuffer(d)
+	f := NewFixedReader(r, 10)
+	for i := 0; i < 5; i++ {
+		g := make([]byte, 2)
+		n, err := f.Read(g)
+		if n != 2 || err != nil {
+			t.Fatal("Expected 2 bytes and no error")
+		}
+		if g[0] != byte(i * 2) || g[1] != byte(i * 2 + 1) {
+			t.Fatal("Expected number sequence")
+		}
+	}
+	g := make([]byte, 2)
+	n, err := f.Read(g)
+	if n != 0 || err == nil {
+		t.Fatal("Expected 0 bytes and error")
+	}
+}
+
+func TestFixedReaderMultiRead(t *testing.T) {
+	d := make([]byte, 20)
+	for i := 0; i < len(d); i++ {
+		d[i] = byte(i)
+	}
+	r := bytes.NewBuffer(d)
+	f := NewFixedReader(r, 10)
+	for i := 0; i < 10; i++ {
+		g := make([]byte, 2)
+		n, err := f.Read(g)
+		if n != 2 || err != nil {
+			t.Fatal("Expected 2 bytes and no error")
+		}
+		if g[0] != byte(i * 2) || g[1] != byte(i * 2 + 1) {
+			t.Fatal("Expected number sequence")
+		}
+	}
+	g := make([]byte, 2)
+	n, err := f.Read(g)
+	if n != 0 || err == nil {
+		t.Fatal("Expected 0 bytes and error")
+	}
+}
+
+func TestFixedReaderTailTruncated(t *testing.T) {
+	d := make([]byte, 4)
+	for i := 0; i < len(d); i++ {
+		d[i] = byte(i)
+	}
+	r := bytes.NewBuffer(d)
+	f := NewFixedReader(r, 3)
+	g := make([]byte, 2)
+	n, err := f.Read(g)
+	if n != 2 || err != nil {
+		t.Fatal("Expected 2 bytes and no error")
+	}
+	if g[0] != 0 || g[1] != 1 {
+		t.Fatal("Expected number sequence")
+	}
+	g2 := make([]byte, 2)
+	n2, err2 := f.Read(g2)
+	if n2 != 1 || err2 != nil {
+		t.Fatal("Expected 1 byte and no error")
+	}
+	if g2[0] != 2 {
+		t.Fatal("Expected number sequence")
+	}
+	g3 := make([]byte, 2)
+	n3, err3 := f.Read(g3)
+	if n3 != 1 || err3 != nil {
+		t.Fatal("Expected 1 byte and no error")
+	}
+	if g3[0] != 3 {
+		t.Fatal("Expected number sequence")
+	}
+}
+
+type closeableBuffer struct {
+	reader io.Reader
+	closed bool
+}
+
+func (b *closeableBuffer) Read(p []byte) (int, error) {
+	if b.closed {
+		return 0, io.EOF
+	} else {
+		return b.reader.Read(p)
+	}
+}
+
+func (b *closeableBuffer) Close() error {
+	b.closed = true
+	return nil
+}
+
+func TestFixedReaderClose(t *testing.T) {
+	d := make([]byte, 4)
+	for i := 0; i < len(d); i++ {
+		d[i] = byte(i)
+	}
+	r := &closeableBuffer{bytes.NewBuffer(d),false}
+	f := NewFixedReader(r, 2)
+	g := make([]byte, 2)
+	n, err := f.Read(g)
+	if n != 2 || err != nil {
+		t.Fatal("Expected 2 bytes and no error")
+	}
+	if g[0] != 0 || g[1] != 1 {
+		t.Fatal("Expected number sequence")
+	}
+	f.Close()
+	g3 := make([]byte, 2)
+	n3, err3 := f.Read(g3)
+	if n3 != 0 || err3 == nil {
+		t.Fatal("Expected 0 bytes and error")
+	}
+}
+
+func TestFixedReaderCloseRemain(t *testing.T) {
+	d := make([]byte, 6)
+	for i := 0; i < len(d); i++ {
+		d[i] = byte(i)
+	}
+	r := &closeableBuffer{bytes.NewBuffer(d),false}
+	f := NewFixedReader(r, 4)
+	g := make([]byte, 2)
+	n, err := f.Read(g)
+	if n != 2 || err != nil {
+		t.Fatal("Expected 2 bytes and no error")
+	}
+	if g[0] != 0 || g[1] != 1 {
+		t.Fatal("Expected number sequence")
+	}
+	f.Close()
+	g2 := make([]byte, 2)
+	n2, err2 := f.Read(g2)
+	if n2 != 2 || err2 != nil {
+		t.Fatal("Expected 2 bytes and no error")
+	}
+	if g2[0] != 2 || g2[1] != 3 {
+		t.Fatal("Expected number sequence")
+	}
+	g3 := make([]byte, 2)
+	n3, err3 := f.Read(g3)
+	if n3 != 0 || err3 == nil {
+		t.Fatal("Expected 0 bytes and error")
+	}
+}
