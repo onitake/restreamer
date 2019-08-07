a68a273bef9670366a7de4b73df30394b422a0b8
Gregor Riepl <onitake@gmail.com>
2019-08-07 19:48:20 +0200

    Unified absSub unit test between 32/64 archs

--- util/abs_test.go
+++ util/abs_test.go
@@ -20,6 +20,8 @@ import (
 	"testing"
 )
 
+const intSize = 4 + (^uint(0)>>63) * 4
+
 func TestAbsSubInt32(t *testing.T) {
 	va := []int32{ -0x7ffffffe, 0x7ffffffe, -0x7fffffff, 0x7fffffff, -2, 2, -1, -1, 0, 0 }
 	vb := []int32{ -0x7ffffffe, 0x7ffffffe, -0x7fffffff, 0x7fffffff, -2, 2, -1, -1, 0, 0 }
@@ -50,6 +52,35 @@ func TestAbsSubInt64(t *testing.T) {
 			b := vb[ib]
 			r := AbsSubInt64(a, b)
 			var x int64
+			if a > b {
+				x = a - b
+			} else {
+				x = b - a
+			}
+			if r != x {
+				t.Errorf("AbsSub(%d, %d) should be %d, got %d", a, b, x, r)
+			}
+		}
+	}
+}
+
+func TestAbsSub(t *testing.T) {
+	v64 := []int64{ -0x7ffffffffffffffe, 0x7ffffffffffffffe, -0x7fffffffffffffff, 0x7fffffffffffffff }
+	va := []int{ -0x7ffffffe, 0x7ffffffe, -0x7fffffff, 0x7fffffff, -2, 2, -1, -1, 0, 0 }
+	vb := []int{ -0x7ffffffe, 0x7ffffffe, -0x7fffffff, 0x7fffffff, -2, 2, -1, -1, 0, 0 }
+	if intSize == 8 {
+		for _, a64 := range v64 {
+			va = append(va, int(a64))
+			vb = append(vb, int(a64))
+		}
+	}
+	for ia := 0; ia < len(va); ia++ {
+		for ib := 0; ib < len(vb); ib++ {
+			a := va[ia]
+			b := vb[ib]
+			t.Logf("Testing |%d - %d|", a, b)
+			r := AbsSub(a, b)
+			var x int
 			if a > b {
 				x = a - b
 			} else {
